af1506d3307105aa89a373f2ae6f80d05e314608
Mikael Klevengård <mickekleven@outlook.com>
2022-10-09 17:49:11 +0200

    Add logotype to navbar

--- MPark.Client/ClientApp/src/components/navbar.js
+++ MPark.Client/ClientApp/src/components/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Management Central
+          <img src="./logotype.svg" width="40"></img> Management Central
         </a>
         <button
           className="navbar-toggler"
@@ -19,7 +19,7 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
                 Home
